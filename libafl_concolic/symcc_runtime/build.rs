078ab11e13432a097e1064ca860bf349afe6b919
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2024-12-18 09:57:13 +0000

    use static lib

--- libafl_concolic/symcc_runtime/build.rs
+++ libafl_concolic/symcc_runtime/build.rs
@@ -259,8 +259,11 @@ fn build_and_link_symcc_runtime(symcc_src_path: &Path, rename_header_path: &Path
         .build()
         .join("lib");
     link_with_cpp_stdlib();
+    if !cpp_lib.join("SymRuntimeStatic.a").exists() {
+        panic!("SymCC runtime build failed: {}", cpp_lib.display());
+    }
     println!("cargo:rustc-link-search=native={}", cpp_lib.display());
-    println!("cargo:rustc-link-lib=static=SymRuntime");
+    println!("cargo:rustc-link-lib=static=SymRuntimeStatic");
 }
 
 fn link_with_cpp_stdlib() {
